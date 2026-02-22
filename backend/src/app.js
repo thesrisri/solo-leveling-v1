131791c7ad0fb9fe33ee873c23cdd236be61f675
Sridharam Srikanth <sridharamsrikanth@gmail.com>
2026-02-22 05:39:46 +0530

    update cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const pool = require("./config/db");
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://solo-leveling-v1-frontend.vercel.app/"],
+    origin: ["http://localhost:5173","https://solo-leveling-v1-frontend.vercel.app"],
     credentials: true,
   })
 );
